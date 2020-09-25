7bf9ef9c1e3ce890b8d55dab8774d3593732f90e
Gabor Neorcsity <neog399@gmail.com>
2020-09-25 15:47:30 +0100

    Return live order summary

--- src/main/kotlin/org/credit/suisse/crypto/excercise/CryptoOrderManager.kt
+++ src/main/kotlin/org/credit/suisse/crypto/excercise/CryptoOrderManager.kt
@@ -1,9 +1,22 @@
 package org.credit.suisse.crypto.excercise
 
+import org.javamoney.moneta.format.CurrencyStyle
+import java.math.BigDecimal
 import java.util.*
+import javax.money.MonetaryAmount
+import javax.money.format.AmountFormatQueryBuilder
+import javax.money.format.MonetaryAmountFormat
+import javax.money.format.MonetaryFormats
 
 class CryptoOrderManager {
-    val orders: MutableMap<UUID, CryptoOrder> = mutableMapOf()
+    private val monetaryFormat: MonetaryAmountFormat = MonetaryFormats.getAmountFormat(
+        AmountFormatQueryBuilder.of(Locale.UK)
+            .set(CurrencyStyle.SYMBOL)
+            .set("pattern", "¤####.##")
+            .build()
+    )
+
+    private val orders: MutableMap<UUID, CryptoOrder> = mutableMapOf()
 
     // Add validation? is coin type supported, is the quantity and price between accepted thresholds, etc.
     fun placeOrder(order: CryptoOrder): UUID {
@@ -17,4 +30,39 @@ class CryptoOrderManager {
     fun cancelOrder(orderId: UUID) {
         orders.remove(orderId)
     }
+
+    // From the exercise, it was unclear whether SELL and BUY orders should be displayed together
+    // or whether orders for different coin types should be merged together
+    // For the sake of simplicity, I chose the conservative approach and displayed them separately
+    fun liveOrderSummaries(orderType: CryptoOrderType, coinType: String): List<String> {
+        val quantitiesPrice: MutableMap<MonetaryAmount, QuantityPerPrice> = mutableMapOf()
+
+        // The merging and sorting could also be done when placing the order (synchronously or asynchronously, depending on specifics)
+        // That would significantly speed up and simplify this method
+        // This would be especially beneficial in a system where we have a lot more reads than writes
+        for ((_, order) in orders) {
+            if (order.type !== orderType || order.coinType !== coinType) {
+                continue
+            }
+
+            quantitiesPrice.compute(order.pricePerCoin) { _, quantityPerPrice ->
+                if (quantityPerPrice == null) {
+                    QuantityPerPrice(order.quantity, order.pricePerCoin)
+                } else {
+                    QuantityPerPrice(quantityPerPrice.quantity.plus(order.quantity), quantityPerPrice.price)
+                }
+            }
+        }
+
+        val sortedQuantitiesPerPrice = if (orderType == CryptoOrderType.SELL) {
+            quantitiesPrice.values.sortedBy { it.price }
+        } else {
+            quantitiesPrice.values.sortedByDescending { it.price }
+        }
+
+        // In a real-life system we'd have pagination here, but again, tried to keep things simple
+        return sortedQuantitiesPerPrice.map { "${it.quantity} for ${monetaryFormat.format(it.price)}" }
+    }
+
+    internal data class QuantityPerPrice(val quantity: BigDecimal, val price: MonetaryAmount)
 }
--- src/test/kotlin/org/credit/suisse/crypto/excercise/CryptoOrderManagerTest.kt
+++ src/test/kotlin/org/credit/suisse/crypto/excercise/CryptoOrderManagerTest.kt
@@ -19,14 +19,12 @@ class CryptoOrderManagerTest {
 
     @Test
     fun placesNewOrder() {
-        val orderId = cryptoOrderManager.placeOrder(order)
+        cryptoOrderManager.placeOrder(order)
 
-        val savedOrder = cryptoOrderManager.orders[orderId]!!
-        assertEquals(order.type, savedOrder.type)
-        assertEquals(order.userId, savedOrder.userId)
-        assertEquals(order.coinType, savedOrder.coinType)
-        assertEquals(order.quantity, savedOrder.quantity)
-        assertEquals(order.pricePerCoin, savedOrder.pricePerCoin)
+        assertEquals(
+            listOf("10.25 for £123.68"),
+            cryptoOrderManager.liveOrderSummaries(CryptoOrderType.SELL, "Ethereum")
+        )
     }
 
     @Test
@@ -35,6 +33,156 @@ class CryptoOrderManagerTest {
 
         cryptoOrderManager.cancelOrder(orderId)
 
-        assertFalse(cryptoOrderManager.orders.containsKey(orderId))
+        assertEquals(
+            emptyList<String>(),
+            cryptoOrderManager.liveOrderSummaries(CryptoOrderType.SELL, "Ethereum")
+        )
+    }
+
+    @Test
+    fun returnsSummaryOfLiveEthereumSellOrders() {
+        // The test data provided in the exercise was missing the coin type for most inputs
+        // so I assumed 'Ethereum' for each input
+        val ethereumSellOrder1 = CryptoOrder(
+            CryptoOrderType.SELL,
+            "user1",
+            "Ethereum",
+            BigDecimal("350.1"),
+            Money.of(BigDecimal("13.6"), "GBP")
+        )
+        val ethereumSellOrder2 = CryptoOrder(
+            CryptoOrderType.SELL,
+            "user2",
+            "Ethereum",
+            BigDecimal("50.5"),
+            Money.of(BigDecimal("14"), "GBP")
+        )
+        val ethereumSellOrder3 = CryptoOrder(
+            CryptoOrderType.SELL,
+            "user3",
+            "Ethereum",
+            BigDecimal("441.8"),
+            Money.of(BigDecimal("13.9"), "GBP")
+        )
+        val ethereumSellOrder4 = CryptoOrder(
+            CryptoOrderType.SELL,
+            "user4",
+            "Ethereum",
+            BigDecimal("3.5"),
+            Money.of(BigDecimal("13.6"), "GBP")
+        )
+        val buyOrder = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user2",
+            "Ethereum",
+            BigDecimal("10.25"),
+            Money.of(BigDecimal("13.9"), "GBP")
+        )
+        val otherCoinOrder = CryptoOrder(
+            CryptoOrderType.SELL,
+            "user1",
+            "Litecoin",
+            BigDecimal("2.89"),
+            Money.of(BigDecimal("12.5"), "GBP")
+        )
+
+        cryptoOrderManager.placeOrder(ethereumSellOrder1)
+        cryptoOrderManager.placeOrder(ethereumSellOrder2)
+        cryptoOrderManager.placeOrder(ethereumSellOrder3)
+        cryptoOrderManager.placeOrder(ethereumSellOrder4)
+        cryptoOrderManager.placeOrder(buyOrder)
+        cryptoOrderManager.placeOrder(otherCoinOrder)
+
+        val summaries: List<String> = cryptoOrderManager.liveOrderSummaries(CryptoOrderType.SELL, "Ethereum")
+
+        assertEquals(3, summaries.size)
+        assertEquals("353.6 for £13.6", summaries[0])
+        assertEquals("441.8 for £13.9", summaries[1])
+        assertEquals("50.5 for £14", summaries[2])
+    }
+
+    @Test
+    fun returnsSummaryOfLiveBitcoinBuyOrders() {
+        val bitcoinBuyOrder1 = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user1",
+            "Bitcoin",
+            BigDecimal("350.1"),
+            Money.of(BigDecimal("13.6"), "GBP")
+        )
+        val bitcoinBuyOrder2 = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user2",
+            "Bitcoin",
+            BigDecimal("50.5"),
+            Money.of(BigDecimal("14"), "GBP")
+        )
+        val bitcoinBuyOrder3 = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user3",
+            "Bitcoin",
+            BigDecimal("441.8"),
+            Money.of(BigDecimal("13.9"), "GBP")
+        )
+        val bitcoinBuyOrder4 = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user4",
+            "Bitcoin",
+            BigDecimal("3.5"),
+            Money.of(BigDecimal("13.6"), "GBP")
+        )
+        val sellOrder = CryptoOrder(
+            CryptoOrderType.SELL,
+            "user2",
+            "Bitcoin",
+            BigDecimal("10.25"),
+            Money.of(BigDecimal("13.9"), "GBP")
+        )
+        val otherCoinOrder = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user1",
+            "Litecoin",
+            BigDecimal("2.89"),
+            Money.of(BigDecimal("12.5"), "GBP")
+        )
+
+        cryptoOrderManager.placeOrder(bitcoinBuyOrder1)
+        cryptoOrderManager.placeOrder(bitcoinBuyOrder2)
+        cryptoOrderManager.placeOrder(bitcoinBuyOrder3)
+        cryptoOrderManager.placeOrder(bitcoinBuyOrder4)
+        cryptoOrderManager.placeOrder(sellOrder)
+        cryptoOrderManager.placeOrder(otherCoinOrder)
+
+        val summaries: List<String> = cryptoOrderManager.liveOrderSummaries(CryptoOrderType.BUY, "Bitcoin")
+
+        assertEquals(3, summaries.size)
+        assertEquals("50.5 for £14", summaries[0])
+        assertEquals("441.8 for £13.9", summaries[1])
+        assertEquals("353.6 for £13.6", summaries[2])
+    }
+
+    @Test
+    fun returnEmptySummaryIfNotResults() {
+        val bitcoinBuyOrder1 = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user",
+            "Bitcoin",
+            BigDecimal("3.5"),
+            Money.of(BigDecimal("13.6"), "GBP")
+        )
+        val bitcoinBuyOrder2 = CryptoOrder(
+            CryptoOrderType.BUY,
+            "user",
+            "Bitcoin",
+            BigDecimal("10.25"),
+            Money.of(BigDecimal("13.9"), "GBP")
+        )
+
+        cryptoOrderManager.placeOrder(bitcoinBuyOrder1)
+        cryptoOrderManager.placeOrder(bitcoinBuyOrder2)
+
+        val summaries: List<String> = cryptoOrderManager.liveOrderSummaries(CryptoOrderType.BUY, "Ethereum")
+
+        assertEquals(emptyList<String>(), summaries)
     }
 }
